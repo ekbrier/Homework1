220151b3d528f7defda1dca6d0357358c1ec3efb
ekbrier <86619513+ekbrier@users.noreply.github.com>
2021-07-14 19:56:04 +0100

    Update Assessment_2_Eleanor_Brierley.py

--- Assessment_2_Eleanor_Brierley.py
+++ Assessment_2_Eleanor_Brierley.py
@@ -1,3 +1,33 @@
+#QUESTION 4
+import unittest
+from unittest import TestCase,  main
+from Assessment_2_Eleanor_Brierley import palindrome_detector
+
+class Test_Palindrome(unittest.TestCase):
+
+    def test_if_palindrome_is_true(self):
+        value = palindrome_detector().is_palindrome('noon')
+        self.assertEquals(value, True)
+
+    def test_if_palindrome_is_false(self):
+        value = palindrome_detector().is_palindrome('banana')
+        self.assertEquals(value, False)
+
+    def test_error_reverse(self):
+        list_of_bad_value = [123, None]
+        for bad_value in list_of_bad_value:
+            self.assertRaises(
+                TypeError,
+                palindrome_detector().reverse,
+                bad_value
+            )
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
+    
+# QUESTION 9
 def two_numbers(my_numbers, target_sum):
     output = []
     for x in range(0, len(my_numbers) - 1):
