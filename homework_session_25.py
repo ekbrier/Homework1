6eba9ca12d0d14bb28b70fe351b1a38632796f3d
ekbrier <86619513+ekbrier@users.noreply.github.com>
2021-08-09 17:46:33 +0100

    Add files via upload
    Homework for session 25

---
+++ homework_session_25.py
@@ -0,0 +1,26 @@
+from collections import Counter
+
+
+def generate_phrase(characters, phrase):
+    character_list = list(characters)
+    phrase_list = list(phrase)
+
+    if len(characters) < len(phrase):
+        print('False')
+        return
+
+    if not (Counter(phrase_list)) - (Counter(character_list)):
+        print('True')
+    else:
+        print('False')
+
+
+# testing code number 1
+characters = 'acpalapi:rmionorg(eeth    '
+phrase = 'rip geronimo the alpaca :('
+generate_phrase(characters, phrase)
+
+# testing code number 2
+characters = "cbacba"
+phrase = "aabbccc"
+generate_phrase(characters, phrase)
